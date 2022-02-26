2ae6bcbf2896f3fd2dbfb77c06cdb5420db46b4b
Destiny AA <destinaye721@gmail.com>
2022-02-26 09:54:22 +0800

    Initial Commit

---
+++ LuckyWood/LuckyWoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckyWoodApp.swift
+//  LuckyWood
+//
+//  Created by Jeffrey Cheung on 26/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct LuckyWoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LuckyWoodUITests/LuckyWoodUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LuckyWoodUITestsLaunchTests.swift
+//  LuckyWoodUITests
+//
+//  Created by Jeffrey Cheung on 26/2/2022.
+//
+
+import XCTest
+
+class LuckyWoodUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
