e8350cd78aa5cd814cf2d65de400180390ad2527
Jeffrey Cheung <jeffreycheung1995@gmail.com>
2022-05-22 23:47:38 -0400

    - popup will be shown when pressed

--- LuckyWood/ContentView.swift
+++ LuckyWood/ContentView.swift
@@ -10,20 +10,66 @@ import SwiftUI
 struct ContentView: View {
     @State var screenWidth: CGFloat = UIScreen.main.bounds.width
     @State var screenHeight: CGFloat = UIScreen.main.bounds.height
+    @State private var isPopupShow: Bool = false
+    
     var body: some View {
-        let player = PlayViewModel()
-        Button(action: {
-            print("---Pressed---")
-            player.play(fileName: "woodSound", fileType: "m4a")
-        }, label: {
-            Text("")
-                .frame(width: screenWidth, height: screenHeight, alignment: .center)
-        })
-            .frame(width: screenWidth, height: screenHeight, alignment: .center)
-            .background(content: {
-                Image("wood1")
-                    .scaledToFit()
+        ZStack {
+            let player = PlayViewModel()
+            Button(action: {
+                print("---Pressed---")
+                player.play(fileName: "woodSound", fileType: "m4a")
+                self.isPopupShow = true
+            }, label: {
+                Text("")
+                    .frame(width: screenWidth, height: screenHeight, alignment: .center)
             })
+                .frame(width: screenWidth, height: screenHeight, alignment: .center)
+                .background(content: {
+                    Image("wood1")
+                        .scaledToFit()
+                })
+            if $isPopupShow.wrappedValue {
+                ZStack {
+                    Color.white
+                    VStack {
+                        Text("Pressed")
+                            .foregroundColor(.black)
+                            .padding()
+                        Spacer()
+                        Button(action: {
+                            self.isPopupShow = false
+                            print("---Dismiss pressed---")
+                        }, label: {
+                            Text("Dismiss")
+                        })
+                    }.padding()
+                }
+                .frame(width: 300, height: 200)
+                .cornerRadius(20).shadow(radius: 20)
+            }
+        }.padding()
+//        let player = PlayViewModel()
+//        Button(action: {
+//            print("---Pressed---")
+//            player.play(fileName: "woodSound", fileType: "m4a")
+//            self.isPopupShow = true
+//        }, label: {
+//            Text("")
+//                .frame(width: screenWidth, height: screenHeight, alignment: .center)
+//        })
+//            .frame(width: screenWidth, height: screenHeight, alignment: .center)
+//            .background(content: {
+//                Image("wood1")
+//                    .scaledToFit()
+//            })
+//        if $isPopupShow.wrappedValue {
+//            ZStack {
+//                Color.white
+//                VStack {
+//                    Text("Pop up")
+//                }
+//            }.padding()
+//        }
     }
 }
 
